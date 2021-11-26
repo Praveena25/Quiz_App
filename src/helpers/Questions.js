1a2fc8d7152872c7b3e0d750d1dd868bbe55d5cb
Your Name <praveenagovind25@gmail.com>
2021-11-26 01:16:58 -0800

    init commit

---
+++ src/helpers/Questions.js
@@ -0,0 +1,43 @@
+export const Questions = [
+    {
+        prompt: "What is my name?",
+        optionA: "Praveena",
+        optionB: "Jack",
+        optionC: "Rose",
+        optionD: "Santhosh",
+        asnwer: "optionA",
+    },
+    {
+        prompt: "What is my pet name?",
+        optionA: "swagy",
+        optionB: "milky",
+        optionC: "honey",
+        optionD: "sweety",
+        asnwer: "optionD",
+    },
+    {
+        prompt: "Which of this is not a programming language?",
+        optionA: "Python",
+        optionB: "JavaScript",
+        optionC: "MOSQ",
+        optionD: "Java",
+        asnwer: "optionC",
+    },
+    {
+        prompt: "Which of this is not a javascript framework?",
+        optionA: "React",
+        optionB: "Angular",
+        optionC: "Vue",
+        optionD: "Java",
+        asnwer: "optionD",
+    },
+    {
+        prompt:" What is the full form of SQL?",
+
+        optionA:"Structured Query List",
+        optionB: "Structure Query Language",
+        optionC:"Sample Query Language",
+        optionD:  "None of these",
+        asnwer: "optionB",
+    },
+];
